899be150815e0d5e8517d1a644b87c9e15557181
Xavi Amoros <xaviamoros@gmail.com>
2017-01-10 18:48:54 +0100

    Clean up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 require('./styles.scss') // The page is now styled
 
-
-import makeAccordion from './accordion.js';
 import Accordion2 from './accordion2.js';
  
 //allow HMR
@@ -18,28 +16,26 @@ if (document.querySelectorAll && document.body.classList){
 
   // Array functions don't apply well to NodeLists
   for(i = 0; i < accordions.length; i++) {
-    //makeAccordion(accordions[i]);
     var accordion = new Accordion2(accordions[i]);
   }
 
   //accordion.addSection('Section 4 (NEW)','This is the new section body');  
 
-  setTimeout(fetchNewSection, 1000);
-
-
-  function fetchNewSection(){
-    console.log("FETCH new section!")
-
-    //insert new section using Ajax
-    fetch('xavieramoros.github.io/newsection.json', {
-      method: 'get'
-    }).then(function(response) {
-      accordion.addSection(response.title,response.body);  
-    }).catch(function(err) {
-      // Error :(
-    });  
-  }
-
-  
+  setTimeout(fetchNewSection, 1000);  
 }
+
+function fetchNewSection(){
+  console.log("FETCH new section!")
+
+  //insert new section using Ajax
+  fetch('xavieramoros.github.io/newsection.json', {
+    method: 'get'
+  }).then(function(response) {
+    var newSection = JSON.parse(response)
+    accordion.addSection( newSection.title , newSection.body );  
+  }).catch(function(err) {
+    console.log('Something went wrong fetching new section')
+  });  
+}
+
 
